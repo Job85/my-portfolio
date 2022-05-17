02524cab4d20c31c19d341022f6e6f2209bf9469
Job85 <julianjernigan@ymail.com>
2022-05-17 11:42:53 -0400

    reverted home page to about page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import ProjectPage from './pages/ProjectPage'
-import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Nav />
       </header>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route path='/projects' element={<ProjectPage />} />
       </Routes>
     </div>
