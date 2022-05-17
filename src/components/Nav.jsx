9c38c59330a5554f9f2d5506d3c95d69b9365172
Job85 <julianjernigan@ymail.com>
2022-05-17 18:16:54 -0400

    added img for profile pic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom'
-
+import WebImage from '../images/WebImage'
+// import pic from '../HeadShot.jpeg'
 const Nav = () => {
 
     return (
-        <nav className='navbar'>
-            <div>
-                <Link to='projects'>Projects</Link>
-                <Link to='about'>About</Link>
-                <Link to='resume'>Resume</Link>
-            </div>
-        </nav>
+        <div>
+            <header>
+                <WebImage />
+            </header>
+            <nav className='navbar'>
+                <div>
+                    <Link to='projects'>Projects</Link>
+                    <Link to='about'>About</Link>
+                    <Link to='resume'>Resume</Link>
+                </div>
+            </nav>
+        </div>
     )
 }
 
---
+++ src/images/WebImage.js
@@ -0,0 +1,7 @@
+import pic from '.images/mypic.jpg';
+
+const WebImage = () => {
+    return <img src={pic} />
+}
+
+export default WebImage
