d65c8abfd98f98c61cf8f2e66d5471020913af78
Job85 <julianjernigan@ymail.com>
2022-05-17 10:52:26 -0400

    created Nav component; placed in header

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+
+    return (
+        <nav className='navbar'>
+            <h2>nav bar</h2>
+            <div>
+                <Link />
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
