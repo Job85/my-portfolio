dbb6e30abd79883386bd41ee99ad2c7e3e5c7a9d
Job85 <julianjernigan@ymail.com>
2022-05-17 23:25:07 -0400

    added resume link

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
 
     return (
         <div>
-            <a>Resume</a>
+            <a href="https://docs.google.com/document/d/e/2PACX-1vSiBcnyO1rayLM3WTUw2zhnm5lqbf3cd5wkEdOO3V1gzI2n8FyTaSRQ1wl8E-XH3GhEvEE2c9FnOLAl/pub">Resume</a>
         </div>
     )
 
