b856393d3b2e15d4d5dfee157010569c51176b56
Job85 <julianjernigan@ymail.com>
2022-05-31 12:34:30 -0400

    touched file PageStyles, imported pic1,2,3 to project page

---
+++ src/pages/PageStyles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Img2 = styled.img`
+    box-sizing: border-box;
+    display: inline;
+    width: 50%;
+    height: 50%;
+    object-fit: contain;
+    border-radius: 2%;
+`
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,24 +2,29 @@ import React from "react"
 import { Div } from "../components/ComopnentStyles"
 import FlipCard from '../components/FlipCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles.scss';
+import pic1 from '../images/OneRingTrivia.png'
+import pic2 from '../images/InventoryApp.png'
+import pic3 from '../images/SoccerGramERD.png'
+import { Img2 } from "./PageStyles";
 
 const cards = [
     {
         id: '1',
-        variant: 'hover',
-        front: 'Hover',
+        variant: 'click',
+        front: <Img2 src={pic1} alt='LOTR' />,
         back: 'Back'
     },
     {
         id: '2',
         variant: 'click',
-        front: 'Click',
+        front: <Img2 src={pic2} alt='RIA' />,
         back: 'Back'
     },
     {
         id: '3',
-        variant: 'focus',
-        front: 'Focus',
+        variant: 'click',
+        front: <Img2 src={pic3} alt='Soccer Gram' />,
         back: 'Back'
     }
 ];
