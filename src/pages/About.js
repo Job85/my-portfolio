1a9567e708b21f42219c8262927534fcc5268b68
Job85 <julianjernigan@ymail.com>
2022-06-15 14:58:02 -0400

    added 3d effect to greeting

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,45 @@
 import React from "react";
-import { Div, Paragraph } from "./PageStyles";
+import styled from "styled-components";
+
+const Div = styled.div`
+    margin-top: 8.375rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-left: 10.75rem;
+    padding-right: 10.75rem;
+    font-size: calc(0.6rem + 1vw);
+    color: white;
+    border-color: green;
+
+    @media screen {
+        min-width: 37.5rem
+    }
+`
+const H3 = styled.h3`
+    color: #008000cc;
+    font-size: 4rem;
+    font-family: Arial, Helvetica, sans-serif;
+    text-shadow: 
+        0 1px 0 #ccc, 0 1.5px 0 #ccc,
+        0 2px 0 #ccc, 0 2.5px 0 #ccc,
+        0 3px 0 #ccc, 0 3.5px 0 #ccc,
+        0 4px 0 #ccc, 0 4.5px 0 #ccc,
+        0 20px 30px rgba(153, 255, 153, 5);
+`
+const Paragraph = styled.p`
+    margin-top: 3.125rem;
+    margin-bottom: 1.25rem;
+    color: white;
+`
 
 const About = () => {
 
     return (
         <Div>
-            <h3>
+            <H3>
                 Greetings, from sunny Florida!
-            </h3>
+            </H3>
             <Paragraph>
                 I'm Julian, a Full-Stack Developer from the Tampa Bay Area. And I'm here to help your organization.
                 Coming from a background in hospitality,
